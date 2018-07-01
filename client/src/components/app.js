efbdbba3acd238165dd64c42db8ce2dc5662fd0e
hoideptrai <toilahoi007@gmail.com>
2018-07-01 07:48:41 +0700

    initial

---
+++ client/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+class App extends Component {
+    render () {
+        return (
+            <div>
+                hoi dai cafaffeafffsfsf
+            </div>
+        )
+    }
+}
+
+export default hot (module)(App)
