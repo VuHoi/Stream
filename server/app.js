b8eda7d2ce81940c938781492f9878de264af045
hoi <toilahoi007@gmail.com>
2018-08-18 22:03:09 +0700

    Building for production

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ homeController(app);
 
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../client-app/build/index.html"));
+    res.sendFile(path.join(__dirname+'/client-app/build/index.html'));
 });
 
   
